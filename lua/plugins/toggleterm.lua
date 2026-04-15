17e33770500049886ca77f3ef4a9b0ed2cb51775
Kong <90138804+ItsKong@users.noreply.github.com>
2026-04-16 02:41:35 +0700

    fix keymap for lazagit

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -16,8 +16,15 @@ return {
 				pattern = "term://*",
 				callback = function()
 					local keymapOpts = { buffer = 0 } -- Now 'buffer = 0' correctly refers to the new terminal
-					vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], keymapOpts)
-					vim.keymap.set("t", "jk", [[<C-\><C-n>]], keymapOpts)
+					vim.keymap.set("t", "<Esc>", function()
+						if vim.bo.filetype == "lazygit" then
+							-- Send the raw escape key to Lazygit so it can close its own prompts
+							return "<Esc>"
+						end
+						-- Otherwise, exit Neovim terminal mode normally
+						return "<C-\\><C-n>"
+					end, { expr = true, desc = "Smart escape for terminal" })
+					-- vim.keymap.set("t", "jk", [[<C-\><C-n>]], keymapOpts)
 					vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], keymapOpts)
 					vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], keymapOpts)
 					vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], keymapOpts)
